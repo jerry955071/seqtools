93f989f9eb42c210b129bf3b9ba342f94239347b
Jerry Chu <jerry955071@gmail.com>
2021-01-03 20:05:51 +0800

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -4,16 +4,18 @@
 #' @aliases c2s 
 #' @aliases base_complement 
 #' @aliases reverse_complement
-#'
 
+#' @export s2c
 s2c = function(string){
   return(strsplit(string, '')[[1]])
 }
 
+#' @export c2s
 c2s = function(chr_vector){
   return(paste(chr_vector, collapse = ''))
 }
 
+#' @export base_complement
 base_complement = function(base){
   if(base %in% c('A','T','C','G')){
     comp = list('A'='T', 'T'='A', 'C'='G', 'G'='C',
@@ -24,6 +26,7 @@ base_complement = function(base){
   }
 }
 
+#' @export reverse_complement
 reverse_complement = function(Sequence){
   s = s2c(Sequence)
   o = as.character(sapply(s, base_complement))
