8706596dc6c499dccd71cd73c28fdd201d99bb20
Jerry Chu <jerry955071@gmail.com>
2021-01-05 09:39:50 +0800

    Update avg.q.R

--- R/avg.q.R
+++ R/avg.q.R
@@ -14,9 +14,9 @@ avg.q = function(object, round = T){UseMethod('avg.q')}
 #' @export 
 avg.q.SeqFastq = function(object, round = T){
   x = (gtools::asc(attr(object, 'qscore')) %>% drop)-33
-  -10*log10(mean(10^(x/-10)))
+  o = -10*log10(mean(10^(x/-10)))
   if(round){
-    x = round(x, 1)
+    o = round(o, 1)
   }
-  return(x)
+  return(o)
 }
